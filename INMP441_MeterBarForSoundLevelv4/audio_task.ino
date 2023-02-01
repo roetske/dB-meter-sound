66d23f2d0bcf783ef402798870536503b089c94b
roetske <72330366+roetske@users.noreply.github.com>
2023-02-01 23:32:24 +0100

    Add files via upload

---
+++ INMP441_MeterBarForSoundLevelv4/audio_task.ino
@@ -0,0 +1,41 @@
+void mic_i2s_reader_task(void* parameter) {
+  mic_i2s_init();
+
+  // Discard first block, microphone may have startup time (i.e. INMP441 up to 83ms)
+  size_t bytes_read = 0;
+  i2s_read(I2S_PORT, &samples, SAMPLES_SHORT * sizeof(int32_t), &bytes_read, portMAX_DELAY);
+
+  while (true) {
+    // Block and wait for microphone values from I2S
+    //
+    // Data is moved from DMA buffers to our 'samples' buffer by the driver ISR
+    // and when there is requested ammount of data, task is unblocked
+    //
+    // Note: i2s_read does not care it is writing in float[] buffer, it will write
+    //       integer values to the given address, as received from the hardware peripheral. 
+    i2s_read(I2S_PORT, &samples, SAMPLES_SHORT * sizeof(SAMPLE_T), &bytes_read, portMAX_DELAY);
+
+    TickType_t start_tick = xTaskGetTickCount();
+    
+    // Convert (including shifting) integer microphone values to floats, 
+    // using the same buffer (assumed sample size is same as size of float), 
+    // to save a bit of memory
+    SAMPLE_T* int_samples = (SAMPLE_T*)&samples;
+    for(int i=0; i<SAMPLES_SHORT; i++) samples[i] = MIC_CONVERT(int_samples[i]);
+
+    sum_queue_t q;
+    // Apply equalization and calculate Z-weighted sum of squares, 
+    // writes filtered samples back to the same buffer.
+    q.sum_sqr_SPL = MIC_EQUALIZER.filter(samples, samples, SAMPLES_SHORT);
+
+    // Apply weighting and calucate weigthed sum of squares
+    q.sum_sqr_weighted = WEIGHTING.filter(samples, samples, SAMPLES_SHORT);
+
+    // Debug only. Ticks we spent filtering and summing block of I2S data
+    q.proc_ticks = xTaskGetTickCount() - start_tick;
+
+    // Send the sums to FreeRTOS queue where main task will pick them up
+    // and further calcualte decibel values (division, logarithms, etc...)
+    xQueueSend(samples_queue, &q, portMAX_DELAY);
+  }
+}
